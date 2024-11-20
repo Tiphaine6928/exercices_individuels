624d971822b2eb82a3b479e03394e514fdc26235
Tiphaine <tiphaine.chapuis69@gmail.com>
2024-11-20 11:45:38 +0100

    Première étape faite

---
+++ regex.js
@@ -0,0 +1,5 @@
+const str = "J'utilise les expressions regulière pour retrouver des schémas de text au sein d'une chaine de caractères."
+
+const exp = /\bde\b/g;
+
+console.log(str.match(exp).length);

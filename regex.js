ad9d67c87e61634a51d84f9cbbbf750b69046563
Tiphaine <tiphaine.chapuis69@gmail.com>
2024-11-20 11:49:48 +0100

    Deuxième étape de faite

--- regex.js
+++ regex.js
@@ -1,5 +1,15 @@
-const str = "J'utilise les expressions regulière pour retrouver des schémas de text au sein d'une chaine de caractères."
+// Etape 1
 
-const exp = /\bde\b/g;
+const str1 = "J'utilise les expressions regulière pour retrouver des schémas de text au sein d'une chaine de caractères."
 
-console.log(str.match(exp).length);
+const exp1 = /\bde\b/g;
+
+console.log(str1.match(exp1).length);
+
+// Etape 2
+
+const str2 = "J'utilise les expressions regulière pour retrouver des schémas de text au sein d'une chaine de caractères."
+
+const exp2 = /de | des/g;
+
+console.log(str2.match(exp2).length);

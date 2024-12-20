37484a7fe4bd2a65f064b0993861c47036eba92c
Tiphaine <tiphaine.chapuis69@gmail.com>
2024-12-20 10:38:38 +0100

    Troisième étape des majuscules faite

--- password_checker/passwordchecker.js
+++ password_checker/passwordchecker.js
@@ -1,13 +1,15 @@
 const longueur = /.{8,}/;
-const lettre = /[a-zA-Z]/;
+const minuscule = /[a-z]/;
+const majuscule = /[A-Z]/;
 const nombre = /[0-9]/;
 const specialChar = /[^a-zA-Z0-9]/;
-const password = "abcdefg";
+const password = "P@ssword123";
 
 if (
     longueur.test(password) &&
     nombre.test(password) &&
-    lettre.test(password) &&
+    minuscule.test(password) &&
+    majuscule.test(password) &&
     specialChar.test(password)
 ) {
     console.log("Mot de passe valide : longueur suffisante et diversité de caractères.");
